6ab10f2f9ed850e73e62103dc8f4f3c99abab808
revido <aleks.markovic94@gmail.com>
2016-01-29 22:02:00 +0100

    Create table class

--- src/pt/Table.java
+++ src/pt/Table.java
@@ -1,5 +1,59 @@
 package pt;
 
-public class Table {
+import java.util.ArrayList;
 
+public class Table {
+    ArrayList<String> columns;
+    ArrayList<ArrayList<String>> content;
+
+    public Table() {
+        columns = new ArrayList<>();
+        content = new ArrayList<>();
+    }
+
+    public void addColumn(String name) {
+        columns.add(name);
+    }
+
+    public void addRow(ArrayList<String> row) {
+        content.add(row);
+    }
+
+    public String returnTable() {
+//        String table = top();
+//
+//        String format = "|";
+//        for (int i = 0; i < columns.size(); i++) {
+//            format += "%" + maxColLen(i) + "s" + "|";
+//        }
+//
+//        table += String.format(format,columns);
+
+        String format = "| %4s | %2s | %32s | ";
+    }
+//
+//    public String top() {
+//        String top = "+";
+//        for (int i = 0; i < columns.size(); i++) {
+//            top += repeatChar(maxColLen(i)) + "+";
+//        }
+//        top += "\r\n";
+//    }
+//
+//    private String repeatChar(int times) {
+//        String s = "";
+//        for (int i = 0; i < times; i++) {
+//            s += "-";
+//        }
+//        return s;
+//    }
+//
+//    public int maxColLen(int col) {
+//        int max = 0;
+//        for (int i = 0; i < content.size(); i++) {
+//            int len = content.get(i).get(col).length();
+//            if (len > max)
+//                max = len;
+//        }
+    }
 }
